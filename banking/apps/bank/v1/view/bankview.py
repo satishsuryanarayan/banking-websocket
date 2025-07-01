ca32af6ddb95e8b6bac9782f34d1470721f65d41
satishsuryanarayan <satsur@hotmail.com>
2025-07-01 01:59:15 -0500

    Refactor BankView

--- banking/apps/bank/v1/view/bankview.py
+++ banking/apps/bank/v1/view/bankview.py
@@ -1,34 +1,36 @@
+from typing import Any
+
 from esmerald import APIView, Stream, WebSocket, websocket
 from esmerald.logging import logger
 from pydantic import BaseModel
 
-import banking.apps.bank.v1.view as view
 from banking.apps.bank.v1.dtos.errordto import ErrorDTO
-
+import importlib
 
 class BankView(APIView):
     path = "/bank"
+    module = importlib.import_module("banking.apps.bank.v1.view")
 
     @websocket(path="/")
     async def handle(self, socket: WebSocket) -> None:
         await socket.accept()
         while True:
-            msg = await socket.receive_json()
-            if "view" in msg and "method" in msg:
+            dto = await socket.receive_json()
+            if "view" in dto and "method" in dto:
                 try:
-                    view_cls = getattr(view, msg["view"])
+                    view_cls: APIView = getattr(self.module, dto["view"])
                     logger.info("View class: " + str(view_cls))
-                    method_ref = getattr(view_cls, msg["method"])
+                    method_ref = getattr(view_cls, dto["method"])
                     logger.info("View method: " + str(method_ref))
-                    schema_type = method_ref.__annotations__["dto"]
+                    schema_type: BaseModel = method_ref.__annotations__["dto"]
                     logger.info("Schema type: " + str(schema_type))
-                    return_type = method_ref.__annotations__["return"]
+                    return_type: Any = method_ref.__annotations__["return"]
                     logger.info("Return type: " + str(return_type))
                     if issubclass(return_type, BaseModel):
-                        response: BaseModel = await method_ref(schema_type.model_validate(msg))
+                        response: BaseModel = await method_ref(schema_type.model_validate(dto))
                         await socket.send_json(response.model_dump_json())
                     elif issubclass(return_type, Stream):
-                        response: Stream = await method_ref(schema_type.model_validate(msg))
+                        response: Stream = await method_ref(schema_type.model_validate(dto))
                         async for value in response.iterator:
                             await socket.send_text(value)
                     else:
@@ -37,5 +39,5 @@ class BankView(APIView):
                     response: ErrorDTO = ErrorDTO(detail=repr(err))
                     await socket.send_json(response.model_dump_json())
             else:
-                error: ErrorDTO = ErrorDTO(detail=msg["Invalid DTO"])
+                error: ErrorDTO = ErrorDTO(detail=dto["Invalid DTO"])
                 await socket.send_json(error)
