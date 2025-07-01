765c9e93b0bc1052ff39de67aeb5e48ba9c1cd90
satishsuryanarayan <satsur@hotmail.com>
2025-06-30 22:08:30 -0500

    Use websocketgateway

--- banking/apps/bank/v1/urls.py
+++ banking/apps/bank/v1/urls.py
@@ -1,4 +1,4 @@
-from esmerald import Gateway
+from esmerald import Gateway, WebSocketGateway
 from lilya.middleware import DefineMiddleware
 
 from banking.apps.bank.v1.view.bankview import BankView
@@ -7,5 +7,5 @@ from banking.apps.bank.v1.view.utils.httpbasicauth import HTTPBasicAuth
 
 route_patterns = [
     Gateway(handler=UsersView),
-    Gateway(handler=BankView, middleware=[DefineMiddleware(HTTPBasicAuth)]),
+    WebSocketGateway(handler=BankView, middleware=[DefineMiddleware(HTTPBasicAuth)]),
 ]
