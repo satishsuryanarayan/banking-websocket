e41135b06af2a82f3ff181fb9571381b5c0d579f
satishsuryanarayan <satsur@hotmail.com>
2025-07-01 01:10:22 -0500

    Added logging to BankView

--- banking/apps/bank/v1/view/bankview.py
+++ banking/apps/bank/v1/view/bankview.py
@@ -1,5 +1,5 @@
 from esmerald import APIView, WebSocket, websocket
-
+from esmerald.logging import logger
 import banking.apps.bank.v1.view as view
 from banking.apps.bank.v1.dtos.errordto import ErrorDTO
 
@@ -15,8 +15,11 @@ class BankView(APIView):
             if "view" in msg and "method" in msg:
                 try:
                     view_cls = getattr(view, msg["view"])
+                    logger.info("View class: " + str(view_cls))
                     method_ref = getattr(view_cls, msg["method"])
+                    logger.info("View method: " + str(method_ref))
                     schema_type = method_ref.__annotations__["dto"]
+                    logger.info("Schema type: " + str(schema_type))
                     response = await method_ref(schema_type.model_validate(msg))
                 except Exception as err:
                     response = ErrorDTO(detail=repr(err))
