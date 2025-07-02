7f64eea672f9c1581a1ed429ea8bcd98729e2757
satishsuryanarayan <satsur@hotmail.com>
2025-07-02 13:55:15 -0500

    Fix module namespace

--- banking/urls.py
+++ banking/urls.py
@@ -1,5 +1,5 @@
 from esmerald import Include
 
 route_patterns = [
-    Include(namespace="banking.bank.apps.v1.urls", name="bank", path="/v1"),
+    Include(namespace="banking.apps.bank.v1.urls", name="bank", path="/v1"),
 ]
